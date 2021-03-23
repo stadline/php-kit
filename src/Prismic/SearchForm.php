dc87fa58ce786b96bd6525eea0d7f7c4facc6b39
Rémi <remi.bocquet@stadline.com>
2021-03-23 17:32:53 +0100

    fix : "??" error

--- src/Prismic/SearchForm.php
+++ src/Prismic/SearchForm.php
@@ -270,7 +270,7 @@ class SearchForm
      */
     public function url()
     {
-        $url = $this->form->getAction() . '?' . http_build_query($this->data);
+        $url = $this->form->getAction() . http_build_query($this->data);
         $url = preg_replace('/%5B(?:[0-9]|[1-9][0-9]+)%5D=/', '=', $url);
         return $url;
     }
